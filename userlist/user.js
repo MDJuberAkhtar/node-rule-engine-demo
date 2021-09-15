dbca22bf30524c0c5493a948def749a8482210b9
Juber Akhtar <juber.akhtar@mobiotics.com>
2021-09-15 16:01:59 +0530

    values to check

--- userlist/user.js
+++ userlist/user.js
@@ -2,34 +2,44 @@
 
 const userData = {
   washington: {
+    username: 'washington',
     service: 'kuku tv',
-    videoswatched: 5,
+    videoswatched: 85,
+    rewardpoint: 100,
     assignment: 1,
     createdAt: '2012-02-14'
   },
   jefferson: {
+    username: 'jefferson',
     service: 'kuku tv',
-    videoswatched: 15,
+    videoswatched: 95,
+    rewardpoint: 200,
     assignment: 3,
     createdAt: '2012-02-14'
   },
   lincoln: {
+    username: 'lincoln',
     service: 'kuku tv',
-    videoswatched: 25,
+    videoswatched: 95,
+    rewardpoint: 500,
     assignment: 5,
     createdAt: '2012-02-14'
   }
   ,
   abraham: {
+    username: 'abraham',
     service: 'kuku tv',
     videoswatched: 35,
+    rewardpoint: 1000,
     assignment: 7,
     createdAt: '2012-02-14'
   },
   samson: {
+    username: 'samson',
     service: 'kuku tv',
-    videoswatched: 400,
+    videoswatched: 80,
     assignment: 0,
+    rewardpoint: 1000,
     createdAt: '2012-02-14'
   }
 }
